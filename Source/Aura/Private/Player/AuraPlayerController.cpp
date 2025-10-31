60b0684ac1d44869ae8a135ff8803d7e3991f33c
Zack Neill <149334445+ZNeill23@users.noreply.github.com>
2025-10-31 01:02:24 -0400

    Modifier Magnitude Calculations

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -24,7 +24,7 @@ void AAuraPlayerController::CursorTrace()
 	if (!CursorHit.bBlockingHit) return;
 
 	LastActor = ThisActor;
-	ThisActor = CursorHit.GetActor();
+	ThisActor = Cast<IEnemyInterface>(CursorHit.GetActor());
 
 	/**
 	 * Line trace from cursor. There are several scenarios:
