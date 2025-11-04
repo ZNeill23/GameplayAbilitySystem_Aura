e046c30cae466c5992ea288785208ab434a71c5b
Zack Neill <149334445+ZNeill23@users.noreply.github.com>
2025-11-04 02:07:00 -0500

    Aura Asset Manager

---
+++ Source/Aura/Private/AuraAssetManager.cpp
@@ -0,0 +1,20 @@
+// Copyright Zack Neill
+
+#include "AuraAssetManager.h"
+#include "AuraGameplayTags.h"
+#include "Engine/Engine.h"
+
+UAuraAssetManager& UAuraAssetManager::Get()
+{
+	check(GEngine);
+	
+	UAuraAssetManager* AuraAssetManager = Cast<UAuraAssetManager>(GEngine->AssetManager);
+	return *AuraAssetManager;
+}
+
+void UAuraAssetManager::StartInitialLoading()
+{
+	Super::StartInitialLoading();
+
+	FAuraGameplayTags::InitializeNativeGameplayTags();
+}
