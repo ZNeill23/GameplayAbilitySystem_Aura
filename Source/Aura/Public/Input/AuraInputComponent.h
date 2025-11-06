3be668fd63406a39cbb135dc4a230fc9126e38f2
Zack Neill <149334445+ZNeill23@users.noreply.github.com>
2025-11-06 18:30:44 -0500

    Callbacks for Ability Input

--- Source/Aura/Public/Input/AuraInputComponent.h
+++ Source/Aura/Public/Input/AuraInputComponent.h
@@ -33,7 +33,7 @@ void UAuraInputComponent::BindAbilityActions(const UAuraInputConfig* InputConfig
 
 			if (ReleasedFunc)
 			{
-				BindAction(Action.InputAction, ETriggerEvent::Completed, Object, ReleasedFunc, Action.InputTag)
+				BindAction(Action.InputAction, ETriggerEvent::Completed, Object, ReleasedFunc, Action.InputTag);
 			}
 			
 			if (HeldFunc)
--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -2,14 +2,16 @@
 
 #pragma once
 
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GameplayTagContainer.h"
+#include "AuraPlayerController.generated.h"
+
 class UInputMappingContext;
 class UInputAction;
 struct FInputActionValue;
 class IEnemyInterface;
-
-#include "CoreMinimal.h"
-#include "GameFramework/PlayerController.h"
-#include "AuraPlayerController.generated.h"
+class UAuraInputConfig;
 
 UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
@@ -35,4 +37,11 @@ private:
 	void CursorTrace();
 	IEnemyInterface* LastActor;
 	IEnemyInterface* ThisActor;
+
+	void AbilityInputTagPressed(FGameplayTag InputTag);
+	void AbilityInputTagReleased(FGameplayTag InputTag);
+	void AbilityInputTagHeld(FGameplayTag InputTag);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	TObjectPtr<UAuraInputConfig> InputConfig;
 };
